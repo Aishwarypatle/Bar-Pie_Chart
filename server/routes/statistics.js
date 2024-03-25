4796a50b6d21755e2f12b9f122a49da7ac8a9ae3
Aishwary <aishwarypatle@26gmail.com>
2024-03-25 23:43:56 +0530

    fix: get all api fixed

--- server/routes/statistics.js
+++ server/routes/statistics.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const StatisticsController = require('../Controllers/statisticsController');
 
-// GET statistics data for a selected month
+
 router.get('/statistics', StatisticsController.getStatisticsData);
 
 module.exports = router;
