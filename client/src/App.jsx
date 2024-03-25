19cfe8496eeaa2f05a825ae1b6fca57a5d5c2337
Lakhansingh5 <Lrahangdale97@gmail.com>
2024-03-25 23:32:30 +0530

    committed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,42 @@
 import './App.css'
-
-import { BrowserRouter as Router, Route,Routes , NavLink } from 'react-router-dom';
-import MainComp from "./Views";
-import Dashboard from "./Views/Dashboard/Components/Dashboard";
-import Profile from "./Views/Dashboard/Components/Profile";
+import TableChart from './components/TableChart'
+import TickPlacementBars from './components/TickPlacementBars'
 
 
 function App() {
 
   return (
     <>
-      <h1>  hello</h1>
+      <div className="main">
+        <div className="circle">
+          <h1> Transaction Dashboard</h1>
+        </div>
+
+        <div className="search-panel">
+          <input
+            type="text"
+            placeholder="Search transaction"
+            className="search-bar"
+          />
+          <select className="month" onChange={() => console.log("select")}>
+            <option>Select Month</option>
+            <option>Jan</option>
+            <option>Feb</option>
+            <option>Mar</option>
+            <option>Apr</option>
+            <option>May</option>
+            <option>June</option>
+            <option>July</option>
+            <option>Aug</option>
+            <option>Sep</option>
+            <option>Oct</option>
+            <option>Nov</option>
+            <option>Dec</option>
+          </select>
+        </div>
+        <TableChart />
+        <TickPlacementBars />
+      </div>
     </>
   )
 }
